8451f051fc864c4623b58f16001f8625e91b75fa
asimceman <aceman1@etf.unsa.ba>
2020-05-20 03:46:27 +0200

    Dodana jedna klasa proba commita

---
+++ Implementation/BibliotekaOOAD/BibliotekaOOAD/Models/Osoba.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BibliotekaOOAD.Models
+{
+    public class Osoba
+    {
+        int id;
+        string ime, prezime, email, lozinka;
+
+        public int Id { get => id; set => id = value; }
+        public string Ime { get => ime; set => ime = value; }
+        public string Prezime { get => prezime; set => prezime = value; }
+        public string Email { get => email; set => email = value; }
+        public string Lozinka { get => lozinka; set => lozinka = value; }
+    }
+}
