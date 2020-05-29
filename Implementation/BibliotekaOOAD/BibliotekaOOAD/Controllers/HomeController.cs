ab32bce0872e97af6c9e515d192aec635bdda7e7
asimceman <aceman1@etf.unsa.ba>
2020-05-30 01:00:32 +0200

    dodan registracija view

--- Implementation/BibliotekaOOAD/BibliotekaOOAD/Controllers/HomeController.cs
+++ Implementation/BibliotekaOOAD/BibliotekaOOAD/Controllers/HomeController.cs
@@ -28,6 +28,11 @@ namespace BibliotekaOOAD.Controllers
             return View();
         }
 
+        public IActionResult Register()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
